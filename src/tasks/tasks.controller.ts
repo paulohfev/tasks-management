9df3e449388e6b73c253ae02f180a7bbecaca362
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-10-04 19:04:59 -0300

    Add CreateTaskDto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -15,10 +16,7 @@ export class TasksController {
 
   @Post()
   // @Body ~> the request body that is assigned to the body parameter as an argument
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Task {
-    return this.tasksService.createTask(title, description);
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 }
