94db07d4579b580f2e7fb4b499681598fa10b5a5
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-10-04 15:32:13 -0300

    Add tasks service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  // taskService param is a private property of the controller, therefore we can use in the class
+  constructor(private tasksService: TasksService) {}
+}
