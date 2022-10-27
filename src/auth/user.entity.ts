e45de39a7e1c4ae974709808891b6c01fe9fcb39
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-10-27 10:40:10 -0300

    Make Tasks and Users DB relation

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Task } from "src/tasks/task.entity";
 
 @Entity()
 export class User {
@@ -11,4 +12,8 @@ export class User {
 
   @Column()
   password: string;
+
+  // eager true ~> in fetching the user, we are also fetching tasks with it.
+  @OneToMany(_type => Task, task => task.user, { eager: true })
+  tasks: Task[];
 }
