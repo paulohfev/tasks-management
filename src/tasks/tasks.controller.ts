3548fd0feeecef67cfad85785b4b7b79bdbbd752
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-10-29 18:42:53 -0300

    Restrict deletion of tasks by user

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -41,8 +41,11 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id') id: string): Promise<void> {
-    return this.tasksService.deleteTask(id);
+  deleteTask(
+    @Param('id') id: string,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.tasksService.deleteTask(id, user);
   }
 
   @Patch('/:id/status')
