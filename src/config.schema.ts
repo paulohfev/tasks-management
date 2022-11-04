7bb9498e10004a2a115495e9a513f03e640d6f91
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-11-03 23:04:24 -0300

    Add schema validation and configuration

---
+++ src/config.schema.ts
@@ -0,0 +1,11 @@
+import * as Joi from '@hapi/joi';
+
+// makes the following values required for configuration when starting the application. Values point to env variable values
+export const configValidationSchema = Joi.object({
+  STAGE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(), 
+});
