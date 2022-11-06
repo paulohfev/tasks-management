7222c2d2034fd939e4b5dae031812e094ee4207c
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-11-06 00:57:22 -0300

    Add tests for getTasks method

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,43 @@
+import { Test } from '@nestjs/testing';
+import { TasksRepository } from './tasks.repository';
+import { TasksService } from './tasks.service';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn()
+});
+
+const mockUser = {
+  username: 'Ariel',
+  id: 'someId',
+  password: 'somePassword',
+  tasks: [],
+};
+
+describe('TasksService', () => {
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  // called before every test
+  beforeEach(async () => {
+    // initialize a NestJS module with tasksService and tasksRepository
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TasksService);
+    tasksRepository = module.get(TasksRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and returns the result', async () => {
+      // mockResolvedValue ~> for mocking promises
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      // call tasksService.getTasks, which should then call the repository's getTasks
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(result).toEqual('someValue');
+    });
+  });
+});
