c95c5dbabb175d6a0829d3c451535691b5909450
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-11-06 21:31:16 -0300

    Add getTaskById tests

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,9 +1,12 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { TaskStatus } from './task-status.enum';
 import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 const mockTasksRepository = () => ({
-  getTasks: jest.fn()
+  getTasks: jest.fn(),
+  findOne: jest.fn(),
 });
 
 const mockUser = {
@@ -40,4 +43,25 @@ describe('TasksService', () => {
       expect(result).toEqual('someValue');
     });
   });
+
+  describe('getTaskById', () => {
+    it('calls TasksRepository.findOne and returns the result', async () => {
+      const mockTask = {
+        title: 'Test title',
+        description: 'Test desc',
+        id: 'someId',
+        status: TaskStatus.OPEN,
+      };
+
+      tasksRepository.findOne.mockResolvedValue(mockTask);
+      const result = await tasksService.getTaskById('someId', mockUser);
+      expect(result).toEqual(mockTask);
+    });
+
+    it('calls TasksRepository.findOne and handles an error', async () => {
+      tasksRepository.findOne.mockResolvedValue(null);
+      // we expect the promise to be rejected. Behind the scenes throwing an error in an async func = promise rejected
+      expect(tasksService.getTaskById('someId', mockUser)).rejects.toThrow(NotFoundException);
+    });
+  })
 });
